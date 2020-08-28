3b58d8b9fc9a5c0b5fa7e37f90d3f3b774fc3422
Internsial_node <61084125+InternsialNode@users.noreply.github.com>
2020-08-28 07:56:06 +0700

    Create pacar_program_ai.php
    Program tanya pacar kepada komputer
    kalau ini yang mau clone gpp kok

---
+++ pacar_program_ai.php
@@ -0,0 +1,47 @@
+<?php
+//cetak pertanyaan
+echo "Komputer : kamu punya pacar? ";
+$pacar = fopen("php://stdin","r");
+$verify = trim(fgets($pacar));
+//komputer mulai kepo
+echo "Komputer : siapa? ";
+$siapa = fopen("php://stdin","r");
+$siapa1 = trim(fgets($siapa));
+//ucapan terimakalsih
+echo "Komputer : makasih, aku cinta kamu internsial node by komputer....gabut program".PHP_EOL;
+echo "Komputer : aku cantik ga? ";
+$cantik = fopen("php://stdin","r");
+$cantik1 = trim(fgets($cantik));
+echo "Komputer : Makasih....i love you internsial node".PHP_EOL;
+//cetak gambar
+echo "     ".PHP_EOL;
+echo "   ".PHP_EOL;
+echo " ".PHP_EOL;
+echo "".PHP_EOL;
+echo "".PHP_EOL;
+echo "".PHP_EOL;
+echo "".PHP_EOL;
+echo "".PHP_EOL;
+echo "".PHP_EOL;
+echo "".PHP_EOL;
+echo " ".PHP_EOL;
+echo "  ".PHP_EOL;
+echo "               ".PHP_EOL;
+echo "     I Love You ".PHP_EOL;
+echo "                 ".PHP_EOL;
+echo "      ".PHP_EOL;
+echo "       ".PHP_EOL;
+echo "        ".PHP_EOL;
+echo "         ".PHP_EOL;
+echo "          ".PHP_EOL;
+echo "           ".PHP_EOL;
+echo "            ".PHP_EOL;
+echo "             ".PHP_EOL;
+echo "              ".PHP_EOL;
+echo "               ".PHP_EOL;
+echo "                ".PHP_EOL;
+echo "                 ".PHP_EOL;
+echo "                  ".PHP_EOL;
+echo "                   ".PHP_EOL;
+//udah itu aja
+?>
